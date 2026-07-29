56e860e9608e70a906aa0ca5d8d6d73152ad8531
Gayuth-W <gayuthdemika@gmail.com>
2026-07-29 13:39:31 +0530

    feat: SpanNode model for the dependecy graph

---
+++ src/test/java/com/gayuth/hephaestus/HephaestusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayuth.hephaestus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HephaestusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
